3b501b87481aab35947345af4e73008438522f92
Laurent Montel <montel@kde.org>
2022-09-04 12:41:11 +0200

    Don't try to create empty path.

--- src/dialogs/initdialog.cpp
+++ src/dialogs/initdialog.cpp
@@ -1,9 +1,14 @@
 #include "initdialog.h"
+#include <QPushButton>
 
 InitDialog::InitDialog(Git::Manager *git, QWidget *parent, Qt::WindowFlags f)
     : AppDialog(git, parent, f)
 {
     setupUi(this);
+    connect(lineEditPath, &QLineEdit::textChanged, this, [this](const QString &text) {
+        buttonBox->button(QDialogButtonBox::Ok)->setEnabled(!text.trimmed().isEmpty());
+    });
+    buttonBox->button(QDialogButtonBox::Ok)->setEnabled(false);
 }
 
 QString InitDialog::path() const
