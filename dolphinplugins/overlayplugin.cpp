c5edb8bc976777c27935d6a56fc9d64ba3f29067
Laurent Montel <montel@kde.org>
2022-09-26 09:02:21 +0200

    Remove not used includes

--- dolphinplugins/overlayplugin.cpp
+++ dolphinplugins/overlayplugin.cpp
@@ -6,7 +6,6 @@ SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "overlayplugin.h"
 
-#include "../src/git/gitmanager.h"
 #include <QDebug>
 #include <QFileInfo>
 #include <QUrl>
--- src/dialogs/commitpushdialog.cpp
+++ src/dialogs/commitpushdialog.cpp
@@ -7,10 +7,8 @@ SPDX-License-Identifier: GPL-3.0-or-later
 #include "commitpushdialog.h"
 #include "GitKlientSettings.h"
 #include "actions/changedfileactions.h"
-#include "dialogs/diffdialog.h"
 #include "git/commands/commandcommit.h"
 #include "git/commands/commandpush.h"
-#include "git/gitfile.h"
 #include "git/gitmanager.h"
 #include "runnerdialog.h"
 
