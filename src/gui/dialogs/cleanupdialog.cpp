bf51624ed825f08b4e51ea15e28d19be00b7ae0f
Hamed Masafi <hamed.masafi@gmail.com>
2022-10-09 18:14:06 +0330

    Add missing license tags

--- src/gui/dialogs/cleanupdialog.cpp
+++ src/gui/dialogs/cleanupdialog.cpp
@@ -1,3 +1,9 @@
+/*
+SPDX-FileCopyrightText: 2021 Hamed Masafi <hamed.masfi@gmail.com>
+
+SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
 #include "cleanupdialog.h"
 #include "commands/commandclean.h"
 
