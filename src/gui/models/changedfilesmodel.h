2f92ea3362381f4f82c261bc539edc4be08cb758
Hamed Masafi <hamed.masafi@gmail.com>
2022-11-10 11:35:07 +0330

    Add override

--- src/gui/models/changedfilesmodel.h
+++ src/gui/models/changedfilesmodel.h
@@ -20,6 +20,8 @@ public:
     int rowCount(const QModelIndex &parent) const override;
     int columnCount(const QModelIndex &parent) const override;
     QVariant data(const QModelIndex &index, int role) const override;
+    bool setData(const QModelIndex &index, const QVariant &value, int role) override;
+    Qt::ItemFlags flags(const QModelIndex &index) const override;
     QString filePath(int index) const;
     int size() const;
     QStringList checkedFiles() const;
@@ -44,13 +46,6 @@ private:
 Q_SIGNALS:
     void checkedCountChanged();
 
-    // QAbstractItemModel interface
-public:
-    bool setData(const QModelIndex &index, const QVariant &value, int role);
-
-    // QAbstractItemModel interface
-public:
-    Qt::ItemFlags flags(const QModelIndex &index) const;
 
 private:
     bool mCheckable;
