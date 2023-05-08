9f6fc8853523971b52fa8bdaee75d4a466bc5640
Laurent Montel <montel@kde.org>
2023-05-08 09:56:24 +0200

    Fix extract message in .h file

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cpp` -o $podir/kommit.pot
+$XGETTEXT `find . -name \*.h -o -name \*.cpp` -o $podir/kommit.pot
 rm -f rc.cpp
