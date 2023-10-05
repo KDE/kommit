71011b7aa9b8215fc8af6d921883b3dbf3299e6e
Hamed Masafi <hamed.masafi@gmail.com>
2023-10-05 19:52:22 +0330

    Add missing licenses

--- src/libkommit/observers/cloneobserver.cpp
+++ src/libkommit/observers/cloneobserver.cpp
@@ -1,3 +1,9 @@
+/*
+SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
 #include "cloneobserver.h"
 
 namespace Git
--- src/libkommit/observers/fetchobserver.cpp
+++ src/libkommit/observers/fetchobserver.cpp
@@ -1,3 +1,9 @@
+/*
+SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
 #include "fetchobserver.h"
 #include <git2/oid.h>
 
