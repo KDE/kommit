b416797ba0502c00ca8603eca87adfb8485dcf19
Laurent Montel <montel@kde.org>
2024-05-29 19:25:25 +0200

    Add missing explicit keyword

--- src/libkommitwidgets/widgets/avatarview.h
+++ src/libkommitwidgets/widgets/avatarview.h
@@ -8,7 +8,7 @@ class AvatarView : public QLabel
     Q_OBJECT
 
 public:
-    AvatarView(QWidget *parent = nullptr);
+    explicit AvatarView(QWidget *parent = nullptr);
 
     Q_REQUIRED_RESULT QString userEmail() const;
     void setUserEmail(const QString &userEmail);
