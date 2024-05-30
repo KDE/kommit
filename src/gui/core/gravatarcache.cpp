2d02c269dbef5dc7461c102c2ea1b8960a924b2c
Laurent Montel <montel@kde.org>
2024-05-30 21:34:31 +0200

    Fix mem leak here too

--- src/gui/core/gravatarcache.cpp
+++ src/gui/core/gravatarcache.cpp
@@ -26,17 +26,18 @@ QString GravatarCache::avatarPath(const QString &email)
     if (mAvatarsCache.contains(emailHash))
         return mAvatarsCache[emailHash];
 
-    auto avatarUrl = "https://www.gravatar.com/avatar/" + emailHash;
+    const auto avatarUrl = QStringLiteral("https://www.gravatar.com/avatar/%1").arg(emailHash);
 
     QNetworkRequest request{QUrl{avatarUrl}};
 
     QNetworkReply *reply = mNet.get(request);
-    connect(reply, &QNetworkReply::finished, [&reply]() {
+    connect(reply, &QNetworkReply::finished, [reply]() {
         QFile avatarFile;
-        if (!avatarFile.open(QIODevice::WriteOnly))
-            return;
-        avatarFile.write(reply->readAll());
-        avatarFile.close();
+        if (avatarFile.open(QIODevice::WriteOnly)) {
+            avatarFile.write(reply->readAll());
+            avatarFile.close();
+        }
+        delete reply;
     });
     return {};
 }
