5953fc135c982af525d789331108a017f3fa2ed2
Laurent Montel <montel@kde.org>
2024-05-30 07:32:58 +0200

    Remove unused include

--- src/libkommitwidgets/dialogs/runnerdialog.cpp
+++ src/libkommitwidgets/dialogs/runnerdialog.cpp
@@ -12,8 +12,6 @@ SPDX-License-Identifier: GPL-3.0-or-later
 #include <KLocalizedString>
 #include <QInputDialog>
 
-#include "libkommitwidgets_appdebug.h"
-
 RunnerDialog::RunnerDialog(Git::Manager *git, QWidget *parent)
     : AppDialog(parent)
     , mGitProcess(new QProcess{this})
