9e26bf38d8eaead328174e472c91fa59a3ea571e
Laurent Montel <montel@kde.org>
2024-05-31 13:57:08 +0200

    Disable ok button when it's empty

--- src/libkommitwidgets/dialogs/taginfodialog.cpp
+++ src/libkommitwidgets/dialogs/taginfodialog.cpp
@@ -5,6 +5,7 @@ SPDX-License-Identifier: GPL-3.0-or-later
 */
 
 #include "taginfodialog.h"
+#include <QPushButton>
 
 TagInfoDialog::TagInfoDialog(QWidget *parent)
     : AppDialog(parent)
@@ -13,11 +14,15 @@ TagInfoDialog::TagInfoDialog(QWidget *parent)
     setWindowTitle(i18nc("@title:window", "New tag"));
 
     connect(lineEditTagName, &QLineEdit::textChanged, this, &TagInfoDialog::slotLineEditTagNameTextChanged);
+    connect(lineEditTagName, &QLineEdit::textChanged, this, &TagInfoDialog::updateOkButton);
+    connect(lineEditMessage, &QLineEdit::textChanged, this, &TagInfoDialog::updateOkButton);
     mOkButton = buttonBox->button(QDialogButtonBox::Ok);
+    mOkButton->setEnabled(false);
 }
 
 void TagInfoDialog::updateOkButton()
 {
+    mOkButton->setEnabled(!tagName().trimmed().isEmpty() && !message().trimmed().isEmpty());
 }
 
 QString TagInfoDialog::tagName() const
