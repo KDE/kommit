92ef7b81f272a77f855b7d0f9da855e296265741
Hamed Masafi <hamed.masafi@gmail.com>
2024-07-01 18:15:11 +0330

    Add missing lisences[

--- src/libkommit/caches/commitscache.cpp
+++ src/libkommit/caches/commitscache.cpp
@@ -1,3 +1,9 @@
+/*
+SPDX-FileCopyrightText: 2021 Hamed Masafi <hamed.masfi@gmail.com>
+
+SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
 #include "commitscache.h"
 #include "entities/commit.h"
 #include "gitglobal_p.h"
