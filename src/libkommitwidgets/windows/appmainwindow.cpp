aca32c4c30878d33ff0a0e9d0de58d3a3a255417
Laurent Montel <montel@kde.org>
2024-07-09 13:44:48 +0200

    Remove commented code

--- src/libkommitwidgets/windows/appmainwindow.cpp
+++ src/libkommitwidgets/windows/appmainwindow.cpp
@@ -55,22 +55,6 @@ void AppMainWindow::setVisible(bool visible)
     KXmlGuiWindow::setVisible(visible);
 }
 
-// void AppMainWindow::closeDialog(int resultCode)
-//{
-//     _returnCode = resultCode;
-//     if (_loop && _loop->isRunning())
-//         _loop->quit();
-// }
-
-// void AppMainWindow::closeEvent(QCloseEvent *event)
-//{
-//     qCDebug(KOMMIT_LOG) << Q_FUNC_INFO;
-//     Q_UNUSED(event)
-//     if (_loop && _loop->isRunning())
-//         _loop->quit();
-//     KXmlGuiWindow::closeEvent(event);
-// }
-
 void AppMainWindow::keyPressEvent(QKeyEvent *event)
 {
     if (event->key() == Qt::Key_Escape && mIsModal)
