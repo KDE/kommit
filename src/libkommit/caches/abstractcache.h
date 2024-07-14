410832d8ec39acaa25ad64fc226ca108e162ec8f
Hamed Masafi <hamed.masafi@gmail.com>
2024-07-14 16:44:40 +0000

    Update abstractcache.h

--- src/libkommit/caches/abstractcache.h
+++ src/libkommit/caches/abstractcache.h
@@ -43,7 +43,7 @@ template<class T>
 class OidLookupProvider;
 
 template<class ObjectType, class PtrType>
-class LIBKOMMIT_EXPORT Cache
+class Cache
 {
 public:
     using DataMember = QSharedPointer<ObjectType>;
@@ -69,7 +69,7 @@ protected:
 };
 
 template<class ObjectType, class PtrType>
-class LIBKOMMIT_EXPORT OidCache : public Cache<ObjectType, PtrType>
+class OidCache : public Cache<ObjectType, PtrType>
 {
 public:
     using GitLookupFunc = int (*)(PtrType **, git_repository *, const git_oid *);
