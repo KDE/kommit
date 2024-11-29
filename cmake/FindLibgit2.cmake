179076f5d0d30cdb5bafc33f660f19693260fb18
Hamed Masafi <hamed.masafi@gmail.com>
2024-11-29 17:05:11 +0330

    rename Libgit2::libgit2 lib name

--- cmake/FindLibgit2.cmake
+++ cmake/FindLibgit2.cmake
@@ -21,9 +21,9 @@ if(Libgit2_FOUND)
     set(LIBGIT2_LIBRARIES ${LIBGIT2_LIBRARY})
     set(LIBGIT2_INCLUDE_DIRS ${LIBGIT2_INCLUDE_DIR})
 
-    if(NOT TARGET Libgit2::libgit2)
-        add_library(Libgit2::libgit2 UNKNOWN IMPORTED)
-        set_target_properties(Libgit2::libgit2 PROPERTIES
+    if(NOT TARGET Libgit2)
+        add_library(Libgit2 UNKNOWN IMPORTED)
+        set_target_properties(Libgit2 PROPERTIES
             IMPORTED_LOCATION "${LIBGIT2_LIBRARY}"
             INTERFACE_INCLUDE_DIRECTORIES "${LIBGIT2_INCLUDE_DIR}"
         )
